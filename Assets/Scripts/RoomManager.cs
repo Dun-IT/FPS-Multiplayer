13c6df57f3695a59523d3d603e6b2aec5db165e1
Nguyen Khoa Doan <85565635+Dun-IT@users.noreply.github.com>
2023-12-23 17:52:46 +0700

    first commit

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public static RoomManager instance;
+
+    public GameObject player;
+
+    [Space]
+    public Transform[] spawnPoints;
+
+    [Space]
+    public GameObject roomCam;
+
+    [Space]
+    public GameObject nameUI;
+
+    public GameObject connectingUI;
+
+    private string nickName = "unamed";
+
+
+    [HideInInspector]
+    public int kills = 0;
+    [HideInInspector]
+    public int deaths = 0;
+
+    public string roomNameToJoin = "test";
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void ChangeNickName(string _name)
+    {
+        nickName = _name;
+    }
+
+    public void JoinRoomButtonPressed()
+    {
+        Debug.Log("Connecting...");
+
+        PhotonNetwork.JoinOrCreateRoom(roomNameToJoin, null, null);
+
+        nameUI.SetActive(false);
+        connectingUI.SetActive(true);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        Debug.Log("In a room");
+
+        roomCam.SetActive(false);
+
+        SpawnPlayer();
+    }
+
+    public void SpawnPlayer()
+    {
+        // Them spawn point thi them vao list tren unity
+        Transform spawnPoint = spawnPoints[UnityEngine.Random.Range(0, spawnPoints.Length)];
+
+        GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+
+        _player.GetComponent<PlayerSetup>().IsLocalPlayer();
+        _player.GetComponent<Health>().isLocalPlayer = true;
+
+        _player.GetComponent<PhotonView>().RPC("SetNickName", RpcTarget.AllBuffered, nickName);
+        PhotonNetwork.LocalPlayer.NickName = nickName;
+    }
+
+    public void SetHashes()
+    {
+        try
+        {
+            Hashtable hash = PhotonNetwork.LocalPlayer.CustomProperties;
+
+            hash["kills"] = kills;
+            hash["deaths"] = deaths;
+
+            PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+        }
+        catch
+        {
+            // Do nothing
+        }
+    }
+}
